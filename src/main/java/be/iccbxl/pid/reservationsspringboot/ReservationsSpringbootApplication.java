558ecb69c58f6e0f31a96cff9d8c284e9e56676b
abdsabb <abdsabb65@gmail.com>
2026-02-05 22:06:39 +0100

    Password recovery - Scheduled token deletion

--- src/main/java/be/iccbxl/pid/reservationsspringboot/ReservationsSpringbootApplication.java
+++ src/main/java/be/iccbxl/pid/reservationsspringboot/ReservationsSpringbootApplication.java
@@ -2,12 +2,14 @@ package be.iccbxl.pid.reservationsspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ReservationsSpringbootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ReservationsSpringbootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ReservationsSpringbootApplication.class, args);
+    }
 
 }
