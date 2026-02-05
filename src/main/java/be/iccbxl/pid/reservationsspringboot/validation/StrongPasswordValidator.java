64e5f69e385d04201317e0386bd7059ec60b3230
abdsabb <abdsabb65@gmail.com>
2026-02-06 00:55:21 +0100

    Profile update

--- src/main/java/be/iccbxl/pid/reservationsspringboot/validation/StrongPasswordValidator.java
+++ src/main/java/be/iccbxl/pid/reservationsspringboot/validation/StrongPasswordValidator.java
@@ -4,12 +4,16 @@ import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 public class StrongPasswordValidator implements ConstraintValidator<StrongPassword, String> {
+
     private static final String PASSWORD_PATTERN =
             "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&._-])[A-Za-z\\d@$!%*?&._-]{8,}$";
 
     @Override
     public boolean isValid(String password, ConstraintValidatorContext context) {
-        if (password == null) return false;
+        // Si vide → considéré comme valide (changement ignoré)
+        if (password == null || password.isBlank()) {
+            return true;
+        }
         return password.matches(PASSWORD_PATTERN);
     }
 }
